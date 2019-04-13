d155f386f6e997b5606fd73a7abd544c9b257bd1
178inaba <178inaba@users.noreply.github.com>
2019-04-14 01:38:58 +0900

    Add TestList

--- joyokanji_test.go
+++ joyokanji_test.go
@@ -1,1 +1,14 @@
 package joyokanji
+
+import "testing"
+
+func TestList(t *testing.T) {
+	rs, err := List()
+	if err != nil {
+		t.Fatalf("Should not be fail: %v.", err)
+	}
+
+	if got, want := len(rs), 2136; got != want {
+		t.Fatalf("`List()` length is %d, want %d.", got, want)
+	}
+}
